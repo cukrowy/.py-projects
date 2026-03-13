d6cca5b26e4f532964095251aa5590044b3ee698
Cukier <bchorzepa72@gmail.com>
2026-03-13 12:09:27 +0000

    dodane mnozenie ale cos jest scuffed
    jak mi sie zachce to naprawie

--- math equasions.py
+++ math equasions.py
@@ -3,23 +3,47 @@ import time
 import os
 o = 0
 a = int(input("Ile zadań matematycznych:  "))
- 
-for i in range(a):
-    x = random.randint(1, 99)
-    y = random.randint(1, 99)
+if a <= 0:
+    print('aha')
+if a >= 1:
+    for i in range(a):
+        zadania = random.randint(1, 4)
+        if zadania == 1:
+            x = random.randint(1, 99)
+            y = random.randint(1, 99)
+            os.system('clear')
+            odp = int(input(f'{x} + {y}?:  '))
 
-    odp = int(input(f'{x} + {y}?:  '))
+            if odp == x + y:
+                o = o + 1
+                print("Dobra Odpowiedz")
+                time.sleep(1)
+                os.system('clear')
+            else:
+                print("Zła Odpowiedź")
+                time.sleep(1)
+                os.system('clear')
+        if zadania == 2:
+            x = random.randint(1, 10)
+            y = random.randint(1, 10)
+            os.system('clear')
+            odp = int(input(f'{x} x {y}?:  '))
 
-    if odp == x + y:
-        o = o + 1
-        print("dobra odpowiedz")
-        time.sleep(1)
-        os.system('cls')
-    else:
-        print("źle")
-        time.sleep(1)
-        os.system('cls')
+            if odp == x * y:
+                o = o + 1
+                print("Dobra Odpowiedz")
+                time.sleep(1)
+                os.system('clear')
+            else:
+                print("Zła Odpowiedź")
+                time.sleep(1)
+                os.system('clear')
+            
 time.sleep(0)
-print(f'{o} dobrych odpowiedzi z {a}')
-time.sleep(1)
-input("\n\nNaciśnij enter aby wyjść")
+if a >= 1:
+    print(f'{o} dobrych odpowiedzi z {a}')
+    time.sleep(1)
+    input("\n\nNaciśnij enter aby wyjść")
+else:
+    time.sleep(1)
+    input("\n\nNaciśnij enter aby wyjść")
