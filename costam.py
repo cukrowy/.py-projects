9b99b7bbd0ea40b0c8018eac93d2a886ef415bf9
Cukier <bchorzepa72@gmail.com>
2026-03-26 14:02:11 +0000

    Add random number selection and fix syntax issues
    Fix syntax errors and implement random number selection logic.

---
+++ costam.py
@@ -0,0 +1,28 @@
+import random
+import os 
+import time
+a = 2
+    if a = 2: #przez ile dzielone
+        x = random.randint(1, 10)
+        if x == 1:
+            cyfra == 2
+        if x == 2:
+            cyfra == 4
+        if x == 3:
+            cyfra == 6
+        if x == 4:
+            cyfra == 8
+        if x == 5:
+            cyfra == 10
+        if x == 6:
+            cyfra == 12
+        if x == 7:
+            cyfra == 14
+        if x == 8:
+            cyfra == 16
+        if x == 9:
+            cyfra == 18
+        if x == 10:
+            cyfra == 20
+        pytanie1 = int(input({f}"{cyfra} % {a}? ").replace" ", "")
+        
